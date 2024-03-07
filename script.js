47f5bf4fc5343380cb1570e057595a012c616af5
solzilberman <sol.zilberman@gmail.com>
2024-03-07 13:54:34 -0500

    clean up js

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-function generateExperienceHTML() {
-    const experienceSection = document.getElementById('experience-section');
-    let htmlContent = '<h2>> Experience</h2><div class="experience-section">';
-
-    DATA.Experience.forEach(exp => {
-        htmlContent += `
-        <div class="experience">
-          <div class="experience-title">
-            <span class="title-text">${exp.title}</span>
-            <span class="title-company">${exp.company}</span>
-          </div>
-          <div class="experience-description">
-            <span class="experience-location">${exp.location}</span><br>
-            ${exp.description}
-          </div>
-        </div>
-      `;
-    });
-
-    htmlContent += '</div>';
-    experienceSection.innerHTML = htmlContent;
-}
-
 function generateHTML(type, data) {
     const section = document.getElementById(`${type.toLowerCase()}-section`);
-    // capitalize the first letter of the type
     const titleText = type.charAt(0).toUpperCase() + type.slice(1);
     let htmlContent = `<h2>> ${titleText}</h2><div class="experience-section">`;
-
     data.forEach(item => {
         htmlContent += `
         <div class="experience">
